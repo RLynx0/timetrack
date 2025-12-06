20a899c64d734e7cc988e2a7f7434e7e8a5d0e18
RLynx0 <luc.signin96@gmail.com>
2025-12-06 15:33:19 +0100

    More show implementations

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::{
     fs,
-    io::{Write, stdin, stdout},
+    io::{self, Write, stdin, stdout},
     path::Path,
     process::exit,
     str::FromStr,
@@ -38,8 +38,8 @@ fn main() {
 
 fn handle_ttr_command(opt: &Opt) -> Result<()> {
     match &opt.command {
-        opt::TtrCommand::Start(opts) => handle_start(opts),
-        opt::TtrCommand::End(opts) => handle_end(opts),
+        opt::TtrCommand::Start(opts) => start_activity(opts),
+        opt::TtrCommand::End(opts) => end_activity(opts),
         opt::TtrCommand::Show(opts) => show_entries(opts),
         opt::TtrCommand::Generate(_) => todo!(),
         opt::TtrCommand::Activity(_) => todo!(),
@@ -62,13 +62,13 @@ macro_rules! verbose_print_pretty {
     };
 }
 
-fn handle_start(start_opts: &opt::Start) -> Result<()> {
+fn start_activity(start_opts: &opt::Start) -> Result<()> {
     let config = &get_config()?;
     let activity_name: &str = &start_opts.activity;
 
     let wbs = resolve_wbs(activity_name)?;
 
-    let last_entry = get_last_state_entry(&files::get_entry_file_path()?)?;
+    let last_entry = get_last_entry(&files::get_entry_file_path()?)?;
     let last_attendance = last_entry.as_ref().and_then(|e| e.attendance_type());
     let attendance = start_opts
         .attendance
@@ -119,8 +119,8 @@ fn sanitize_description(description: &str) -> String {
     description.replace("\t", "    ").replace("\n", " -- ")
 }
 
-fn handle_end(end_opts: &opt::End) -> Result<()> {
-    let last_entry = get_last_state_entry(&files::get_entry_file_path()?)?;
+fn end_activity(end_opts: &opt::End) -> Result<()> {
+    let last_entry = get_last_entry(&files::get_entry_file_path()?)?;
     match last_entry.as_ref() {
         Some(ActivityEntry::Start(last_start)) => {
             let entry = ActivityEntry::new_end();
@@ -161,31 +161,51 @@ fn write_entry(entry: &ActivityEntry) -> Result<()> {
 }
 
 fn show_entries(show_opts: &opt::Show) -> Result<()> {
-    if matches!(show_opts.last, LastValue::SingleEntries(1)) {
-        let entry = get_last_state_entry(&files::get_entry_file_path()?)?;
-        match entry {
-            None => println!("You have not recorded any data yet"),
-            Some(ActivityEntry::End(_)) => {
-                println!("You are not tracking any activity")
-            }
-            Some(ActivityEntry::Start(entry)) => {
-                println!(
-                    "Tracking activity \u{001B}[32m'{}'\u{001B}[0m",
-                    entry.name()
-                );
+    match &show_opts.last {
+        LastValue::SingleEntries(1) => show_last_entry(),
+        lval => show_multiple_entries(lval),
+    }
+}
 
-                let delta = Local::now() - entry.time_stamp();
-                verbose_print_pretty! {
-                    true => [
-                        "Description" => entry.description(),
-                        "Attendance" => entry.attendance(),
-                        "WBS" => entry.wbs(),
-                        "Tracked for" => delta,
-                    ]
-                };
-            }
+fn show_last_entry() -> Result<()> {
+    let entry = get_last_entry(&files::get_entry_file_path()?)?;
+    match entry {
+        None => println!("You have not recorded any data yet"),
+        Some(ActivityEntry::End(_)) => {
+            println!("You are not tracking any activity")
+        }
+        Some(ActivityEntry::Start(entry)) => {
+            println!(
+                "Tracking activity \u{001B}[32m'{}'\u{001B}[0m",
+                entry.name()
+            );
+
+            let delta = Local::now() - entry.time_stamp();
+            verbose_print_pretty! {
+                true => [
+                    "Description" => entry.description(),
+                    "Attendance" => entry.attendance(),
+                    "WBS" => entry.wbs(),
+                    "Tracked for" => delta,
+                ]
+            };
         }
     }
+    Ok(())
+}
+
+fn show_multiple_entries(lval: &LastValue) -> Result<()> {
+    let reversed_entries = match lval {
+        LastValue::SingleEntries(n) => get_last_n_entries(&files::get_entry_file_path()?, *n)?,
+        LastValue::Hours(_) => todo!(),
+        LastValue::Days(_) => todo!(),
+        LastValue::Months(_) => todo!(),
+    };
+
+    for entry in reversed_entries.iter().rev() {
+        println!("{entry}")
+    }
+
     Ok(())
 }
 
@@ -247,17 +267,26 @@ fn get_input_string(query: &str) -> Result<String> {
     Ok(input.trim().into())
 }
 
-fn get_last_state_entry(path: &Path) -> Result<Option<ActivityEntry>> {
+fn get_last_entry(path: &Path) -> Result<Option<ActivityEntry>> {
+    get_last_n_entries(path, 1).map(|v| v.into_iter().next())
+}
+
+/// Fetches the last `count` entries from a path in reversed order
+fn get_last_n_entries(path: &Path, count: usize) -> Result<Vec<ActivityEntry>> {
     if !fs::exists(path)? {
-        return Ok(None);
+        return Ok(Vec::new());
     }
     let file = fs::File::open(path)?;
     let mut rev_lines = RawRevLines::new(file);
-    match rev_lines.next() {
-        Some(res) => {
-            let entry = &String::from_utf8(res?)?;
-            Ok(Some(ActivityEntry::from_str(entry)?))
-        }
-        None => Ok(None),
-    }
+    rev_lines
+        .take(count)
+        .map(entry_from_byte_result)
+        .collect::<Result<Vec<_>>>()
+}
+
+fn entry_from_byte_result(
+    byte_result: std::result::Result<Vec<u8>, io::Error>,
+) -> Result<ActivityEntry> {
+    let entry_str = String::from_utf8(byte_result?)?;
+    Ok(ActivityEntry::from_str(&entry_str)?)
 }
